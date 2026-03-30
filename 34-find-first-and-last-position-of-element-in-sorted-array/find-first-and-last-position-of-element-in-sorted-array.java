c62780b47e70402f7e7d7d51c7a8e14809f6dd3c
Prabhat <prabhatyadav.dbg@gmail.com>
2026-03-30 21:31:11 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (62.30%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,17 +1,52 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = -1, last = -1;
+        int first = lowerBound(nums, target);
+        
+        // if target not found
+        if (first == nums.length || nums[first] != target) {
+            return new int[]{-1, -1};
+        }
 
-        for (int i = 0; i<nums.length; i++) {
+        int last = upperBound(nums, target) - 1;
 
-            if(nums[i] == target) {
-                if(first == -1) {
-                    first = i;
-                }
-                 last = i;
+        return new int[]{first, last};
+    }
+
+    // first occurrence
+    private int lowerBound(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = nums.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return new int[]{first, last};
-        
+
+        return ans;
+    }
+
+    // first index greater than target
+    private int upperBound(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = nums.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
     }
 }

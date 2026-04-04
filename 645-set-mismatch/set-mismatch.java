9713a28670d63a58919ffb0630ab5604ae864ba5
Prabhat <prabhatyadav.dbg@gmail.com>
2026-04-04 17:40:34 +0530

    Time: 18 ms (15.58%) | Memory: 47.5 MB (72.99%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,20 +1,38 @@
+import java.util.*;
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int[] result = new int[2];
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        int duplicate = -1;
+        int missing = -1;
 
-        for(int num : nums) {
-            map.put(num, map.getOrDefault(num,0)+1);
+       
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        for (int i=1; i<=nums.length; i++) {
-            if (map.getOrDefault(i,0) == 2) {
-                result[0] = i;
+        
+        for (int i = 1; i <= nums.length; i++) {
+            if (map.getOrDefault(i, 0) == 2) {
+                duplicate = i;
             }
             if (!map.containsKey(i)) {
-                result[1] = i; 
+                missing = i;
             }
         }
-        return result;
+
+       
+        result.add(duplicate);
+        result.add(missing);
+
+       
+        int[] ans = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            ans[i] = result.get(i);
+        }
+
+        return ans;
     }
 }

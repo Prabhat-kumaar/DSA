77676857227bb7bc458f157864d65633375b426e
Prabhat <prabhatyadav.dbg@gmail.com>
2026-07-15 11:01:03 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (14.66%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left < right) {
+
+            if(nums[left] % 2 == 0 && nums[right] % 2 != 0){
+                left++;
+                right--;
+            }
+            else if(nums[left] % 2 != 0 && nums[right] % 2 == 0){
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+                right--;
+            }
+            else if(nums[left] % 2 == 0 && nums[right] % 2 == 0) {
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return nums; 
+    }
+}
